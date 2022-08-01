fc6cd7d4ec6fb9685e90321fe939bdf497b7ee06
EC2 Default User <ec2-user@ip-172-31-2-162.ap-northeast-1.compute.internal>
2022-08-01 10:42:57 +0000

    完成

---
+++ app/models/search.rb
@@ -0,0 +1,12 @@
+class Search < ApplicationRecord
+#def self.search(keyword)
+#  where(["title like? OR body like?", "%#{keyword}%", "%#{keyword}%"])
+#end
+
+#def search
+#  @posts = Post.search(params[:keyword])
+#  @keyword = params[:keyword]
+#  render "index"
+#end
+
+end
