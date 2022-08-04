bffb5efe9cc9fc04e988bdc59b717abd90cdea5d
EC2 Default User <ec2-user@ip-172-31-2-162.ap-northeast-1.compute.internal>
2022-08-04 06:23:12 +0000

    いいねランキング

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         
   #belongs_to :books
-  has_many :books#n1の関係じゃないとダメ
+  has_many :books, dependent: :destroy#n1の関係じゃないとダメ
   #has_many :post_images, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
