26ab14181776facd6d771fc71fb3d12389d6ae94
EC2 Default User <ec2-user@ip-172-31-2-162.ap-northeast-1.compute.internal>
2022-08-04 07:08:38 +0000

    dm機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,14 @@ class User < ApplicationRecord
   
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-
+  has_many :user_rooms #チャット機能の記述ここから
+  has_many :chats
+  has_many :rooms, through: :user_rooms #ここまでを追記！
+  
   # 一覧画面で使う
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
